b1f6fce41cb8c597dd7e9693eaaa23988b752754
Guido Cruz Huanca <guido_cruz_huanca@outlook.com>
2021-04-11 11:52:32 -0400

    data binding with some examples.[F

--- src/app/web-components/lista-usuarios/lista-usuarios.component.ts
+++ src/app/web-components/lista-usuarios/lista-usuarios.component.ts
@@ -7,8 +7,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListaUsuariosComponent implements OnInit {
 
+  tittle = "Informacion a mostrar"
+  data_two_way_headers = ["Name", "LastName"]
+  data_two_way_content = [
+    {
+      "name": "Juanito", 
+      "lastName": "Perez"
+    },
+    {
+      "name": "Jose", 
+      "lastName": "Perez"
+    },
+    {
+      "name": "Andra", 
+      "lastName": "Santivanhiez"
+    },
+    {
+      "name": "Daniela", 
+      "lastName": "Sanjines"
+    }
+  ]
+
   constructor() { }
 
+  agregarNuevoElementoADataTwoWay(): void {
+    var nuevoElemento = {
+      "name": "NuevoItemName", 
+      "lastName": "NuevoElemnt:astName"
+    }
+
+    this.data_two_way_content.push(nuevoElemento)
+  }
+
+  funcionDeTS(): void {
+    console.log("the function 'nuevaFuncion' was executed.")
+  }
+  
   ngOnInit(): void {
   }
 
