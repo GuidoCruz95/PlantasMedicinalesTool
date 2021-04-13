e0ab9d5cc92d137299679e5bf26cd0a0887e800a
RicardoLara5272 <ricardojose5272@gmail.com>
2021-04-13 19:00:43 -0400

    creado el modulo de ver listas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { AppComponent } from './app.component';
 
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { ListaUsuariosComponent } from './web-components/lista-usuarios/lista-usuarios.component';
+import { VistaListaPlantasComponent } from './vista-lista-plantas/vista-lista-plantas.component';
 
 @NgModule({
   imports: [
@@ -28,7 +29,8 @@ import { ListaUsuariosComponent } from './web-components/lista-usuarios/lista-us
   declarations: [
     AppComponent,
     AdminLayoutComponent,
-    ListaUsuariosComponent
+    ListaUsuariosComponent,
+    VistaListaPlantasComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
