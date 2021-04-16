f79b0c829ab2ad644ee6ab6afb4e1affceb3d07b
riderrojas17 <riderrojas17@gmail.com>
2021-04-16 01:38:57 -0400

    adicion de conexion a base de datos

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,15 @@
 // The list of which env maps to which file can be found in `angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase:{
+    apiKey: "AIzaSyDWUH5I8xakoiL6LPU5O4Yat8a9PSnKuDE",
+    authDomain: "plantas-medicinales-5e724.firebaseapp.com",
+    databaseURL: "https://plantas-medicinales-5e724-default-rtdb.firebaseio.com",
+    projectId: "plantas-medicinales-5e724",
+    storageBucket: "plantas-medicinales-5e724.appspot.com",
+    messagingSenderId: "219202834813",
+    appId: "1:219202834813:web:94631da0f6569c1168fc14",
+    measurementId: "G-W0HQSLZ33E"
+  }
 };
