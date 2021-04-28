c129ea0b1ff489bb4201b00d18f9f68c503a1c15
Guido Cruz Huanca <guido_cruz_huanca@outlook.com>
2021-04-27 23:30:02 -0400

    updating angular

--- src/app/web-components/vista-lista-plantas/vista-lista-plantas.component.spec.ts
+++ src/app/web-components/vista-lista-plantas/vista-lista-plantas.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { VistaListaPlantasComponent } from './vista-lista-plantas.component';
 
@@ -6,7 +6,7 @@ describe('VistaListaPlantasComponent', () => {
   let component: VistaListaPlantasComponent;
   let fixture: ComponentFixture<VistaListaPlantasComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ VistaListaPlantasComponent ]
     })
