20c1ea4ef1cc391673ff37626bd3d39f50affb60
Paisa2 <67492040+Paisa2@users.noreply.github.com>
2021-05-09 20:01:43 -0400

    validacion_rafael

--- src/app/web-components/login/login.component.ts
+++ src/app/web-components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-// import { FormControl, FormGroup } from '@angular/forms';
+import { UsuarioServiceService } from '../../services/usuario-service.service';
+import { Router } from '@angular/router'
 
 @Component({
   selector: 'app-login',
@@ -8,26 +9,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  // loginForm= new FormGroup({
-  //     email: new FormControl(""),
-  //     password: new FormControl(""),
-  // });
-  // authSvc: any;
-  // constructor() { }
+  ussername = ""
+  password = ""
 
-  ngOnInit(): void {}
+  usuarios = []
 
-  // async onLogin(){
+  constructor(private usuarioServiceService: UsuarioServiceService,
+    private router: Router) { 
+    }
 
-  //   //console.log("Form->", this.loginForm.value);
-  //   const{emial, password} = this.loginForm.value;
-  //   try {
-  //     this.authSvc.login(emial, password);
-      
-  //   } catch (error) {
-  //     console.log(error);
-      
-  //   }
+  login(): any{
+    if(this.valid_users()){
+      this.router.navigate(['./vista-lista-plantas']);
+    }
+  }
 
-  //  }
+  valid_users(): boolean {
+    for (var _i = 0; _i < this.usuarios.length; _i++) {
+      let user = this.usuarios[_i]
+      if (user.email == this.ussername && user.password == this.password ){
+        console.log("Bienvenido "+user.name)
+        return true;
+      }
+    }
+    console.log("Sus datos no son correctos")
+    return false;
+  }
+
+  ngOnInit(): void {
+    this.usuarioServiceService.getUsers().subscribe(res=>{
+      this.usuarios = res.map(a => {
+        const data = a.payload.doc.data();
+        data.id = a.payload.doc.id;
+        return data;
+      })
+    }
+    );
+  }
 }
