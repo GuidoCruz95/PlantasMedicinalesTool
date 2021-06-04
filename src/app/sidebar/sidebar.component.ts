6993e74566535adebca3cfb00bbebc08b2654ebb
Paisa2 <67492040+Paisa2@users.noreply.github.com>
2021-06-04 12:51:47 -0400

    agregando-botones

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -10,9 +10,9 @@ declare interface RouteInfo {
 export const ROUTES: RouteInfo[] = [
   { path: '/lista-usuarios', title: 'Lista de usuarios',  icon: 'pe-7s-user', class: '' },
   { path: '/vista-lista-plantas', title: 'Lista de plantas',  icon: 'pe-7s-albums', class: '' },
-  { path: '/login', title: 'Admin_Login',  icon: 'pe-7s-safe', class: '' },
+  { path: '/login', title: 'Login',  icon: 'pe-7s-safe', class: '' },
   { path: '/insertar-plantas', title: 'insertar-plantas',  icon: 'pe-7s-bottom-arrow', class: '' },
-  { path: '/registrar-user', title: 'registrar usuario',  icon:'pe-7s-note2', class: '' },
+  { path: '/registrar-user', title: 'Formulario',  icon:'pe-7s-note2', class: '' },
     // { path: '/dashboard', title: 'tmp-Dashboard',  icon: 'pe-7s-graph', class: '' },
     // { path: '/user', title: 'tmp-User Profile',  icon:'pe-7s-user', class: '' },
     // { path: '/table', title: 'tmp-Table List',  icon:'pe-7s-note2', class: '' },
